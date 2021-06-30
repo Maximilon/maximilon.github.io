dd04a5acb0f1221bcca03072c3e097a886fb5648
Maximilon-Baddeley <Maximilonbaddeley@gmail.com>
2021-07-01 10:35:15 +1200

    add final version 1.0

--- blog/js/app.js
+++ blog/js/app.js
@@ -47,3 +47,27 @@ document.addEventListener("keydown", function (e) {
     closeModal();
   }
 });
+
+//functionaity for sticky header
+// When the user scrolls the page, execute myFunction
+window.onscroll = function () {
+  stickHeader();
+};
+
+// // Get the header
+// const header = document.querySelector(".header");
+// const headerPlaceholder = document.querySelector(".headerPlaceholder");
+
+// // Get the offset position of the navbar
+// let sticky = header.offsetTop;
+
+// // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+// function stickHeader() {
+//   if (window.pageYOffset > sticky) {
+//     header.classList.add("sticky");
+//     headerPlaceholder.styles.display = "block";
+//   } else {
+//     header.classList.remove("sticky");
+//     headerPlaceholder.styles.display = "none";
+//   }
+// }
