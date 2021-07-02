05078607c3510229b7ec6bcdd1904b83d7bf8932
Maximilon-Baddeley <Maximilonbaddeley@gmail.com>
2021-07-02 15:59:01 +1200

    add modal for blog 5

--- js/app.js
+++ js/app.js
@@ -6,16 +6,19 @@ const modalOne = document.querySelector(".modalOne");
 const modalTwo = document.querySelector(".modalTwo");
 const modalThree = document.querySelector(".modalThree");
 const modalFour = document.querySelector(".modalFour");
+const modalFive = document.querySelector(".modalFive");
 
 const btnCloseModal1 = document.querySelector(".closeModal1");
 const btnCloseModal2 = document.querySelector(".closeModal2");
 const btnCloseModal3 = document.querySelector(".closeModal3");
 const btnCloseModal4 = document.querySelector(".closeModal4");
+const btnCloseModal5 = document.querySelector(".closeModal5");
 
 const openModalOne = document.querySelectorAll(".openModalOne");
 const openModalTwo = document.querySelectorAll(".openModalTwo");
 const openModalThree = document.querySelectorAll(".openModalThree");
 const openModalFour = document.querySelectorAll(".openModalFour");
+const openModalFive = document.querySelectorAll(".openModalFive");
 
 const body = document.querySelector("body");
 const overlay = document.querySelector(".overlay");
@@ -44,11 +47,18 @@ const openModal4 = function () {
   body.classList.add("modalScroll");
 };
 
+const openModal5 = function () {
+  modalFive.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+  body.classList.add("modalScroll");
+};
+
 const closeModal = function () {
   modalOne.classList.add("hidden");
   modalTwo.classList.add("hidden");
   modalThree.classList.add("hidden");
   modalFour.classList.add("hidden");
+  modalFive.classList.add("hidden");
   overlay.classList.add("hidden");
   body.classList.remove("modalScroll");
 };
@@ -71,6 +81,11 @@ overlay.addEventListener("click", closeModal);
 for (let i = 0; i < openModalFour.length; i++)
   openModalFour[i].addEventListener("click", openModal4);
 btnCloseModal4.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
+
+for (let i = 0; i < openModalFive.length; i++)
+  openModalFive[i].addEventListener("click", openModal5);
+btnCloseModal5.addEventListener("click", closeModal);
 overlay.addEventListener("click", closeModal);
 
 document.addEventListener("keydown", function (e) {
